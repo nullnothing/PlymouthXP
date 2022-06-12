4e4495f80e8d9f3ba35b44f5412fbfa5f86191d8
nulln <47261624+nullnothing@users.noreply.github.com>
2022-06-12 22:41:06 +1000

    install: Add note about support for distributions

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# IMPORTANT: This install script has only been tested on Arch Linux.
+# Installation procedure may vary from distribution-to-distribution.
+
 rm -rf /usr/share/plymouth/themes/PlymouthXP
 cp -r $(pwd) /usr/share/plymouth/themes/PlymouthXP
 plymouth-set-default-theme -R PlymouthXP
