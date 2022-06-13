a1259ead5456d0d3d9f25def80a5ac46a42eb38f
nulln <47261624+nullnothing@users.noreply.github.com>
2022-06-13 20:16:04 +1000

    prepare_assets: Create "native shell" background from XP files
    This is preliminary work for displaying boot messages/password prompts in the "native shell" style. If any messages/password prompts arise, the boot screen will swap to this "native shell" style and display messages/prompt for a password there.

--- prepare_assets/prepare_assets.py
+++ prepare_assets/prepare_assets.py
@@ -37,14 +37,14 @@ if not os.path.isfile("ntoskrnl.exe"):
 	error("ntoskrnl.exe not found! This file can be found in C:\\WINDOWS\\system32 from an XP installation.")
 
 # Extract resources from ntoskrnl
-if not os.path.isdir("ntoskrnl_out"):
-	os.mkdir("ntoskrnl_out")
+if not os.path.isdir("temp_dir"):
+	os.mkdir("temp_dir")
 
-os.system("wrestool -x ntoskrnl.exe -t 2 -o ntoskrnl_out")
+os.system("wrestool -x ntoskrnl.exe -t 2 -o temp_dir")
 
 # Apply palette to extracted base & progress images
-os.system("mtpaint --cmd -file/open=ntoskrnl_out/ntoskrnl.exe_2_1.bmp -palette/load=xp.gpl -file/save")
-os.system("mtpaint --cmd -file/open=ntoskrnl_out/ntoskrnl.exe_2_8.bmp -palette/load=xp.gpl -file/save")
+os.system("mtpaint --cmd -file/open=temp_dir/ntoskrnl.exe_2_1.bmp -palette/load=xp.gpl -file/save")
+os.system("mtpaint --cmd -file/open=temp_dir/ntoskrnl.exe_2_8.bmp -palette/load=xp.gpl -file/save")
 
 # Process colorized images
 
@@ -52,7 +52,7 @@ if not os.path.isdir("../images"):
 	os.mkdir("../images")
 
 # Base image: we need to clear the rectangle where the progress image will travel
-main_image = Image.open("ntoskrnl_out/ntoskrnl.exe_2_1.bmp").convert("RGBA")
+main_image = Image.open("temp_dir/ntoskrnl.exe_2_1.bmp").convert("RGBA")
 main_draw = ImageDraw.Draw(main_image)
 main_draw.rectangle((259, 354, 376, 362), fill=(0, 0, 0, 0))
 main_image.save("../images/base.png")
@@ -62,12 +62,31 @@ main_image.save("../images/base.png")
 # apply to the XP boot screen. When the progress image is scaled, it must
 # have that border so that the edges are fuzzy. If they weren't there, it
 # would have hard edges -- not what we want.
-progress_image = Image.open("ntoskrnl_out/ntoskrnl.exe_2_8.bmp").convert("RGBA")
+progress_image = Image.open("temp_dir/ntoskrnl.exe_2_8.bmp").convert("RGBA")
 progress_final_image = Image.new("RGBA", (24, 11), (0, 0, 0))
 progress_final_image.paste(progress_image, (1, 1))
 progress_final_image.save("../images/progress.png")
 
+# Assemble "native shell" background..
+# We need to convert these images to something Pillow is happy with with mtpaint..
+# Seems re-saving is enough
+os.system("mtpaint --cmd -file/open=temp_dir/ntoskrnl.exe_2_6.bmp -file/as=temp_dir/nheader.bmp")
+os.system("mtpaint --cmd -file/open=temp_dir/ntoskrnl.exe_2_7.bmp -file/as=temp_dir/nfootergrad.bmp")
+
+native_header = Image.open("temp_dir/nheader.bmp").convert("RGBA")
+native_footer_gradient = Image.open("temp_dir/nfootergrad.bmp").convert("RGBA")
+
+native_background = Image.new("RGB", (640, 480), (87, 120, 208))
+native_background_draw = ImageDraw.Draw(native_background)
+
+native_background.paste(native_header, (0, 0))
+native_background.paste(native_footer_gradient, (0, 419))
+native_background_draw.rectangle((0, 422, 640, 480), fill=(0, 48, 152))
+
+native_background.save("../images/native_bg.png")
+
+
 # All done. I'm omitting the cleanup step here, and leaving the choice to do so
 # up to the end-user.
 print("All done, assets have been prepared and placed in ../images.")
-print("If you'd like, you can remove the ntoskrnl_out directory.")
+print("If you'd like, you can remove the temp_dir directory.")
